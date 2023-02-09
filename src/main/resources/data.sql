c2a428a2c61d89c53226b96bc445719094c68d3a
boscha1 <bosch.anthony7@gmail.com>
2023-02-09 17:05:17 -0500

    adding more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,177 @@
 INSERT INTO COLOR (id, name) VALUES (1, 'orange');
 INSERT INTO COLOR (id, name) VALUES (2, 'black');
 INSERT INTO COLOR (id, name) VALUES (3, 'white');
-INSERT INTO COLOR (id, name) VALUES (4, 'blue');
-INSERT INTO COLOR (id, name) VALUES (5, 'red');
-INSERT INTO COLOR (id, name) VALUES (6, 'navy');
+INSERT INTO COLOR (id, name) VALUES (4, 'red');
+INSERT INTO COLOR (id, name) VALUES (5, 'blue');
+INSERT INTO COLOR (id, name) VALUES (6, 'purple');
+INSERT INTO COLOR (id, name) VALUES (7, 'metallic gold');
+INSERT INTO COLOR (id, name) VALUES (8, 'gold');
+INSERT INTO COLOR (id, name) VALUES (9, 'brown');
+INSERT INTO COLOR (id, name) VALUES (10, 'dark gold');
+INSERT INTO COLOR (id, name) VALUES (11, 'teal');
+INSERT INTO COLOR (id, name) VALUES (12, 'navy blue');
+INSERT INTO COLOR (id, name) VALUES (13, 'titans blue');
+INSERT INTO COLOR (id, name) VALUES (14, 'silver');
+INSERT INTO COLOR (id, name) VALUES (15, 'speed blue');
+INSERT INTO COLOR (id, name) VALUES (16, 'facemask gray');
+INSERT INTO COLOR (id, name) VALUES (17, 'deep steel blue');
+INSERT INTO COLOR (id, name) VALUES (18, 'battle red');
+INSERT INTO COLOR (id, name) VALUES (19, 'royal blue');
+INSERT INTO COLOR (id, name) VALUES (20, 'aqua');
+INSERT INTO COLOR (id, name) VALUES (21, 'marine blue');
+INSERT INTO COLOR (id, name) VALUES (22, 'nautical blue');
+INSERT INTO COLOR (id, name) VALUES (23, 'new century silver');
+INSERT INTO COLOR (id, name) VALUES (24, 'gotham green');
+INSERT INTO COLOR (id, name) VALUES (25, 'powder blue');
+INSERT INTO COLOR (id, name) VALUES (26, 'sunshine gold');
+
+INSERT INTO STADIUM(id, name) VALUES (1, 'Nippert Stadium');
+INSERT INTO STADIUM(id, name) VALUES (2, 'Riverfront Stadium');
+INSERT INTO STADIUM(id, name) VALUES (3, 'Riverfront Stadium');
+INSERT INTO STADIUM(id, name) VALUES (4, 'Memorial Stadium');
+INSERT INTO STADIUM(id, name) VALUES (5, 'M&T Bank Stadium');
+INSERT INTO STADIUM(id, name) VALUES (6, 'Forbes Field');
+INSERT INTO STADIUM(id, name) VALUES (7, 'Pitt Stadium');
+INSERT INTO STADIUM(id, name) VALUES (8, 'Three Rivers Stadium');
+INSERT INTO STADIUM(id, name) VALUES (9, 'Acrisure Stadium');
+INSERT INTO STADIUM(id, name) VALUES (10, 'Cleveland Stadium');
+INSERT INTO STADIUM(id, name) VALUES (11, 'FirstEnergy Stadium');
+INSERT INTO STADIUM (id, name) VALUES (12, 'TIAA Bank Field');
+INSERT INTO STADIUM (id, name) VALUES (13, 'Nissan Stadium');
+INSERT INTO STADIUM (id, name) VALUES (14, 'RCA Dome');
+INSERT INTO STADIUM (id, name) VALUES (15, 'Lucas Oil Stadium');
+INSERT INTO STADIUM (id, name) VALUES (16, 'NRG Stadium');
+INSERT INTO STADIUM (id, name) VALUES (17, 'War Memorial Stadium');
+INSERT INTO STADIUM (id, name) VALUES (18, 'Highmark Stadium');
+INSERT INTO STADIUM (id, name) VALUES (19, 'Miami Orange Bowl');
+INSERT INTO STADIUM (id, name) VALUES (20, 'Hard Rock Stadium');
+INSERT INTO STADIUM (id, name) VALUES (21, 'Nickerson Field');
+INSERT INTO STADIUM (id, name) VALUES (22, 'Fenway Park');
+INSERT INTO STADIUM (id, name) VALUES (23, 'Alumni Stadium');
+INSERT INTO STADIUM (id, name) VALUES (24, 'Harvard Stadium');
+INSERT INTO STADIUM (id, name) VALUES (25, 'Foxboro Stadium');
+INSERT INTO STADIUM (id, name) VALUES (26, 'Gillette Stadium');
+INSERT INTO STADIUM (id, name) VALUES (27, 'Polo Grounds');
+INSERT INTO STADIUM (id, name) VALUES (28, 'Shea Stadium');
+INSERT INTO STADIUM (id, name) VALUES (29, 'Giants Stadium');
+INSERT INTO STADIUM (id, name) VALUES (30, 'MetLife Stadium');
+INSERT INTO STADIUM (id, name) VALUES (31, 'Cotton Bowl');
+INSERT INTO STADIUM (id, name) VALUES (32, 'Municipal Stadium');
+INSERT INTO STADIUM (id, name) VALUES (33, 'Arrowhead Stadium');
+INSERT INTO STADIUM (id, name) VALUES (34, 'Los Angeles Memorial Coliseum');
+INSERT INTO STADIUM (id, name) VALUES (35, 'Balboa Stadium');
+INSERT INTO STADIUM (id, name) VALUES (36, 'Qualcomm Stadium');
+INSERT INTO STADIUM (id, name) VALUES (37, 'Sun Devil Stadium');
+INSERT INTO STADIUM (id, name) VALUES (38, 'Dignity Health Sports Pack');
+INSERT INTO STADIUM (id, name) VALUES (39, 'SoFi Stadium');
+INSERT INTO STADIUM (id, name) VALUES (40, 'Kezar Stadium');
+INSERT INTO STADIUM (id, name) VALUES (41, 'Candlestick Park');
+INSERT INTO STADIUM (id, name) VALUES (42, 'Frank Youell Field');
+INSERT INTO STADIUM (id, name) VALUES (43, 'Oakland Coliseum');
+INSERT INTO STADIUM (id, name) VALUES (44, 'Allegiant Stadium');
+INSERT INTO STADIUM (id, name) VALUES (45, 'Hilltop Stadium');
+INSERT INTO STADIUM (id, name) VALUES (46, 'Mile High Stadium');
+INSERT INTO STADIUM (id, name) VALUES (47, 'Empower Field at Mile High');
 
 INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
-VALUES (1, 'zac', 'taylor', 33, 38, 1);
+VALUES (1, 'Zac', 'Taylor', 33, 38, 1);
 INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
-VALUES (2, 'sean', 'mcdermott', 66, 40, 0);
+VALUES (2, 'John', 'Harbaugh', 116, 74, 0);
 INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
-VALUES (3, 'bill', 'belichick', 329, 165, 0);
+VALUES (3, 'Mike', 'Tomlin', 128, 80, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (4, 'Kevin', 'Stefanski', 27, 25, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (5, 'Doug', 'Pederson', 56, 48, 1);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (6, 'Mike', 'Vrabel', 50, 37, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (7, 'Jeff', 'Saturday', 1, 7, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (8, 'DeMeco', 'Ryans', 0, 0, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (9, 'Sean', 'McDermott', 66, 40, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (10, 'Mike', 'McDaniel', 9, 8, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (11, 'Bill', 'Belichick', 329, 165, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (12, 'Robert', 'Saleh', 11, 23, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (13, 'Andy', 'Reid', 268, 154, 1);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (14, 'Brandon', 'Staley', 19, 16, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (15, 'Josh', 'McDaniel', 17, 28, 0);
+INSERT INTO HEAD_COACH (id, first_name, last_name, wins, losses, ties)
+VALUES (16, 'Sean', 'Payton', 161, 97, 0);
 
 INSERT INTO LOCATION (id, city, state, state_code, zip_code)
-VALUES (1, 'cincinnati', 'ohio', 'oh', '45202');
+VALUES (1, 'Cincinnati', 'Ohio', 'OH', '45202');
 INSERT INTO LOCATION (id, city, state, state_code, zip_code)
-VALUES (2, 'buffalo', 'new york', 'ny', '14127');
+VALUES (2, 'Baltimore', 'Maryland', 'MD', '21201');
 INSERT INTO LOCATION (id, city, state, state_code, zip_code)
-VALUES (3, 'new england', 'massachusetts', 'ma', '02035');
+VALUES (3, 'Pittsburgh', 'Pennsylvania', 'PA', '15212');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (4, 'Cleveland', 'Ohio', 'OH', '44114');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (5, 'Jacksonville', 'Florida', 'FL', '32202');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (6, 'Nashville', 'Tennessee', 'TN', '37213');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (7, 'Indianapolis', 'Indiana', 'IN', '46225');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (8, 'Houston', 'Texas', 'TX', '46225');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (9, 'Buffalo', 'New York', 'NY', '14127');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (10, 'Miami', 'Florida', 'FL', '33056');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (11, 'New England', 'Massachusetts', 'MA', '02035');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (12, 'New York', 'New York', 'NY', '10001');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (13, 'Kansas City', 'Missouri', 'MO', '64192');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (14, 'Los Angeles', 'California', 'CA', '90301');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (15, 'Las Vegas', 'Nevada', 'NV', '89101');
+INSERT INTO LOCATION (id, city, state, state_code, zip_code)
+VALUES (16, 'Denver', 'Colorado', 'CO', '80014');
 
-INSERT INTO TEAM (id, name, established_date, stadium, fight_song, mascot, location_id, head_coach_id)
-VALUES (1, 'bengals', '1967-05-23', 'paycor stadium', 'the bengals growl', 'who dey', 1, 1);
-INSERT INTO TEAM (id, name, established_date, stadium, fight_song, mascot, location_id, head_coach_id)
-VALUES (2, 'bills', '1959-10-28', 'highmark stadium', 'shout', 'billy buffalo', 2, 2);
-INSERT INTO TEAM (id, name, established_date, stadium, fight_song, mascot, location_id, head_coach_id)
-VALUES (3, 'patriots', '1959-11-16', 'gillette stadium', 'i''m shipping up to boston', 'pat patriot', 3, 3);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (1, 'Bengals', '1967-05-23', 'The Bengals Growl', 'Who Dey', 1, 1);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (2, 'Ravens', '1996-02-14', 'Fight for Old Baltimore', 'Poe', 2, 2);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (3, 'Steelers', '1933-07-08', 'Renegade', 'Steely McBeam', 3, 3);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (4, 'Browns', '1944-06-04', 'Hi-O-Hi-O', 'Brownie the Elf', 4, 4);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (5, 'Jaguars', '1993-11-30', 'Jaxson de Ville', 5, 5);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (6, 'Titans', '1959-08-14', 'T-Rac', 6, 6);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (7, 'Colts', '1953-01-23', 'Blue', 7, 7);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (8, 'Texans', '1999-10-06', 'Toro', 8, 8);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (9, 'Bills', '1959-10-28', 'Shout','Billy Buffalo', 9, 9);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (10, 'Dolphins', '1965-08-16', 'Miami Dolphins #1', 'T.D', 10, 10);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (11, 'Patriots', '1959-11-16', 'I''m Shipping Up To Boston', 'Pat Patriot', 11, 11);
+INSERT INTO TEAM (id, name, established_date, fight_song, location_id, head_coach_id)
+VALUES (12, 'Jets', '1959-08-14', 'Jets Roll With It', 12, 12);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (13, 'Chiefs', '1959-08-14', 'K.C Wolf', 13, 13);
+INSERT INTO TEAM (id, name, established_date, location_id, head_coach_id)
+VALUES (14, 'Chargers', '1959-08-14', 14, 14);
+INSERT INTO TEAM (id, name, established_date, fight_song, mascot, location_id, head_coach_id)
+VALUES (15, 'Raiders', '1960-01-30', 'The Autumn Wind', 'Raider Rusher', 15, 15);
+INSERT INTO TEAM (id, name, established_date, mascot, location_id, head_coach_id)
+VALUES (16, 'Broncos', '1959-08-14', 'Miles', 16, 16);
+
 
 INSERT INTO TEAM_COLORS (team_id, colors_id)
 VALUES  (1, 1);
@@ -33,20 +180,197 @@ VALUES  (1, 2);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
 VALUES  (1, 3);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (2, 3);
+VALUES  (2, 2);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (2, 4);
+VALUES  (2, 6);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (2, 5);
+VALUES  (2, 7);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (3, 3);
+VALUES  (3, 2);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (3, 5);
+VALUES  (3, 8);
 INSERT INTO TEAM_COLORS (team_id, colors_id)
-VALUES  (3, 6);
+VALUES  (4, 1);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (4, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (4, 9);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (5, 2);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (5, 10);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (5, 11);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (6, 12);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (6, 13);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (6, 14);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (6, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (6, 4);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (7, 15);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (7, 16);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (7, 2);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (7, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (8, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (8, 17);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (8, 18);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (9, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (9, 4);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (9, 12);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (9, 19);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (10, 1);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (10, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (10, 20);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (10, 21);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (11, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (11, 4);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (11, 22);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (11, 23);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (12, 2);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (12, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (12, 24);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (13, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (13, 4);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (13, 8);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (14, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (14, 25);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (14, 26);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (15, 2);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (15, 14);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (16, 1);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (16, 3);
+INSERT INTO TEAM_COLORS (team_id, colors_id)
+VALUES  (16, 12);
 
-
-
-
-
-
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (1, 1);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (1, 2);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (1, 3);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (2, 4);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (2, 5);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (3, 6);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (3, 7);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (3, 8);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (3, 9);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (4, 10);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (4, 11);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (5, 12);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (6, 13);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (7, 14);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (7, 15);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (8, 16);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (9, 17);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (9, 18);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (10, 19);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (10, 20);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 21);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 22);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 23);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 24);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 25);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (11, 26);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (12, 27);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (12, 28);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (12, 29);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (12, 30);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (13, 31);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (13, 32);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (13, 33);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 34);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 35);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 36);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 37);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 38);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (14, 39);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 40);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 41);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 42);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 43);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 34);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (15, 44);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (16, 45);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (16, 46);
+INSERT INTO TEAM_STADIUMS (team_id, stadiums_id)
+VALUES (16, 47);
