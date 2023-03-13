5c127400ad3d8d24c9ab23cb8d701a91a541140d
boscha1 <bosch.anthony7@gmail.com>
2023-03-13 10:49:57 -0400

    removing unnecessary services for now

--- src/main/java/com/example/sportsdataservice/model/response/TeamData.kt
+++ src/main/java/com/example/sportsdataservice/model/response/TeamData.kt
@@ -1,7 +1,6 @@
 package com.example.sportsdataservice.model.response
 
 import com.example.sportsdataservice.dto.TeamDTO
-import com.example.sportsdataservice.model.Team
 
 data class TeamData(
     val data: TeamList
